e6885998facb06724fe6b61bd730d01b9cb40c0d
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-07-28 15:27:41 +0300

    Update checkpoint.swift

--- checkpoint.swift
+++ checkpoint.swift
@@ -1,4 +1,6 @@
 //checkpoint3
+//THE solution here 3 && 5 must be at the beginning is crucial to ensure it gets evaluated first and the correct output is printed for multiples of both 3 and 5.
+
 for i in 1...100 {
     if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
         print("Fizz Buzz")
